8ab47d26879e20c6cc5e65e9861131cf149e33f8
sunwook kim <koownusmik@gmail.com>
2019-08-15 10:19:39 +0900

    initial commit
    App.js 에 semantic-ui-react 의 모달 컴포넌트를 붙여 초기화 하였음.

---
+++ README.md
@@ -0,0 +1,7 @@
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## 설계
+
+-RecommandSlider (Q)
+-MenuTable (Q)
+-MenuItem (Wook)
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ModalModalExample from './component/ModalModalExample';
+
+
+function App() {
+  return (
+    <>
+      <span>코드스쿼드 맛집사이트를 만들어보자!</span>
+      <ModalModalExample />
+    </>
+  );
+}
+
+export default App;
