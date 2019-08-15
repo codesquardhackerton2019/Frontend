d3788cea9ec0af6c48c07781eb694816b8993610
sunwook kim <koownusmik@gmail.com>
2019-08-15 10:58:07 +0900

    fixr: data 가져오는 경로 변경

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import RestaurantList from './RestaurantList/RestaurantList';
-import Header from './Header/Header';
-import styled from 'styled-components';
-import Recommended from './Recommendation/Recommended';
-import FooterCont from './Footer/Footer';
-import RefreshBtn from './RestaurantList/RefreshBtn';
+import React, { useState, useEffect } from "react";
+import RestaurantList from "./RestaurantList/RestaurantList";
+import Header from "./Header/Header";
+import styled from "styled-components";
+import Recommended from "./Recommendation/Recommended";
+import FooterCont from "./Footer/Footer";
+import RefreshBtn from "./RestaurantList/RefreshBtn";
 
 const Wrapper = styled.div`
-    /* background-color: #EACD85; */
-`
+  /* background-color: #EACD85; */
+`;
 
 const Div = styled.div`
   width: 70%;
   margin: 0 auto;
   margin-top: 50px;
-`
+`;
 
 const Todays = styled.p`
   box-shadow: 0 1px 1px black, 0 1px 1px black;
@@ -24,35 +24,36 @@ const Todays = styled.p`
   width: 300px;
   text-shadow: 2px 2px 2px black;
   margin-top: 40px;
-`
+`;
 
 const App = () => {
-
   const [restaurantsInfo, setRestaurantsInfo] = useState([]);
-  
+
   const getRestaurants = async () => {
-    const restaurantsJson = await fetch('http://192.168.1.4:3000/api-docs/').then(data => data.json());
+    const restaurantsJson = await fetch(
+      `${process.env.PUBLIC_URL}/json/localData.json`
+    ).then(data => data.json());
     const allRestaurantsInfo = restaurantsJson.body;
     setRestaurantsInfo(allRestaurantsInfo);
-  }
+  };
 
   useEffect(() => {
     getRestaurants();
-  }, [])
+  }, []);
 
   return (
     <Wrapper>
       <Header />
       <Todays>오늘의 베스트</Todays>
-      <Recommended restaurantsInfo={restaurantsInfo}/>
+      <Recommended restaurantsInfo={restaurantsInfo} />
       <Todays>오늘의 랜덤봇</Todays>
       <Div>
         {/* <RefreshBtn onClick={} /> */}
-        <RestaurantList restaurantsInfo={restaurantsInfo}/>
+        <RestaurantList restaurantsInfo={restaurantsInfo} />
       </Div>
       <FooterCont />
     </Wrapper>
-  )
-}
+  );
+};
 
 export default App;
